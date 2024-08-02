f2ba25be70e2feeeff24c27f3c99e5f05322dbe3
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-02 23:29:18 +1000

    output client as standalone app

--- apps/client/src/module/system/theme-store.ts
+++ apps/client/src/module/system/theme-store.ts
@@ -7,17 +7,11 @@ type ThemeStore = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
 };
-const themeData =
-  typeof window !== undefined ? localStorage.getItem("theme-store") : null;
 
 export const useThemeStore = create(
   persist<ThemeStore>(
     (set) => ({
-      theme: themeData
-        ? themeData.includes("dark")
-          ? "dark"
-          : "light"
-        : "dark",
+      theme: "dark",
       setTheme: (theme) => set({ theme }),
     }),
     {
