cc87dd35a3cf9bf59211a85cafb91210b252c87c
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-03 10:33:25 +1000

    add util script to run turbo cmds

---
+++ scripts/turbo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+CMD=$1
+FILTER=$2
+NAMESPACE=@beequant
+
+if [ -n "$CMD" ]; then
+  case $CMD in
+  "dev")
+    if [ -n "$FILTER" ]; then
+      case $FILTER in
+      "client")
+        yarn turbo run dev --filter=$NAMESPACE/client --filter=$NAMESPACE/platform-api
+        ;;
+
+      *)
+        yarn turbo run dev --filter=$NAMESPACE/$FILTER
+        ;;
+      esac
+    else
+      yarn turbo run dev
+    fi
+    ;;
+
+  "ci")
+    yarn turbo run lint --force
+    yarn turbo run type-check --force
+    yarn turbo run build --force
+    ;;
+
+  *)
+    yarn turbo run $CMD
+    ;;
+
+  esac
+else
+  echo "No command provided"
+fi
