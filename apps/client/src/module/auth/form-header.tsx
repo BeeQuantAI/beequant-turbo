2e6a0063f5ac50314914a1fb4b5da1c310fd6eb4
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-06 21:16:52 +1000

    style login form as per style guide

--- apps/client/src/module/auth/form-header.tsx
+++ apps/client/src/module/auth/form-header.tsx
@@ -2,11 +2,11 @@ export function FormHeader() {
   return (
     <div className="border-primary-400 flex flex-col border-l-4 px-3 dark:text-neutral-200">
       <span className="text-2xl">Welcome to</span>
-      <span className="text-2xl font-bold">
+      <span className="text-2xl font-bold tracking-wide">
         {"BeeQuant "}
         <span className="text-primary-400">AI</span>
       </span>
-      <span className="text-xs text-neutral-700 dark:text-neutral-400">
+      <span className="text-xs font-light text-neutral-700 dark:text-neutral-400">
         Trading smart, trading with BeeQuant AI
       </span>
     </div>
