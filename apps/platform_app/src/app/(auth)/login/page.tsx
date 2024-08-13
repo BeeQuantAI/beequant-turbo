7d5b332ae5f132172709da441ea06ce7deb0239d
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-13 11:00:16 +1000

    discard old apps from repo

--- apps/platform_app/src/app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import LoginForm from 'module/auth/login-form/LoginForm';
-
-const Login = () => {
-  return <LoginForm />;
-};
-
-export default Login;
--- apps/platform_app/src/config/config.ts
+++
@@ -1,3 +0,0 @@
-export const config = {
-  referenceName: process.env.VITE_APP_REFERENCE_NAME || 'Default_Reference_Name',
-};
