87817615683d96eaf66ddd43f6e3237a4338a73a
Shiyu <chengjjy16@gmail.com>
2024-08-13 14:27:20 +1000

    Added primary and accent color palettes, and subsequently updated the styles for the login form, register form, buttons, checkboxes, and layout.

--- apps/client/src/module/auth/form-header.tsx
+++ apps/client/src/module/auth/form-header.tsx
@@ -1,10 +1,10 @@
 export function FormHeader() {
   return (
-    <div className="border-primary-400 flex flex-col border-l-4 px-3 dark:text-neutral-200">
+    <div className="border-accent-250 flex flex-col border-l-4 px-3 dark:text-neutral-200">
       <span className="text-2xl">Welcome to</span>
       <span className="text-2xl font-bold tracking-wide">
         {"BeeQuant "}
-        <span className="text-primary-400">AI</span>
+        <span className="text-accent-250">AI</span>
       </span>
       <span className="text-xs font-light text-neutral-700 dark:text-neutral-400">
         Trading smart, trading with BeeQuant AI
