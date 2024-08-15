62b1032da1484373cbff72bfc8a7bab04a4c8efb
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-15 22:59:28 +1000

    add simple message about empty root page

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center p-4">
-      main page
+    <main className="flex min-h-screen w-full flex-col items-center justify-center p-4">
+      <span>
+        {
+          "If you are seeing this, it most likely means you are authenticated. (having a \"token\" cookie, we don't check if it's valid btw)."
+        }
+      </span>
+      <span>You probably want to go to the /dashboard page at this stage.</span>
     </main>
   );
 }
