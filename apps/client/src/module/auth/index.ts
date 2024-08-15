68e1faaa87c7703de9e804be66574ac2c8345a08
Shiyu <chengjjy16@gmail.com>
2024-08-15 14:06:00 +1000

    create the 200 page

--- apps/client/src/module/auth/index.ts
+++ apps/client/src/module/auth/index.ts
@@ -1,6 +1,7 @@
 export { useLogout } from "./auth-service";
 export * from "./login-form";
 export * from "./register-form";
+export * from "./register-successed";
 export * from "./route";
 export * from "./verify-email-page";
 export * from "./forgot-password-form";
