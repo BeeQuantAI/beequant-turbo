9f764e95da2231c4cdcb63e34f711895b23da634
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-17 11:48:30 +1000

    supress inconsistent attribute warning on html tag

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link
           rel="stylesheet"
