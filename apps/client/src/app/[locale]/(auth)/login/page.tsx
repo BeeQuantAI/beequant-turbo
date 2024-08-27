0de0812d7d88e165417de56d70939d649e3cda6a
Benchen997 <benchen.melbournechen0422@gmail.com>
2024-08-27 12:43:13 +1000

    resolve: suspense boundary issue posed by Next.js in /login.page.tsx

--- apps/client/src/app/[locale]/(auth)/login/page.tsx
+++ apps/client/src/app/[locale]/(auth)/login/page.tsx
@@ -2,6 +2,7 @@ import { AuthRoute, LoginForm } from "@src/module/auth";
 
 export const metadata = AuthRoute.Login.Metadata;
 
-export default function Page() {
-  return <LoginForm />;
+export default function Page({ searchParams }: {searchParams: { [key: string]: string | string[] | undefined }}) {
+  const token = searchParams.token as string;
+  return <LoginForm token={token}/>;
 }
