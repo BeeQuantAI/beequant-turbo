bb6f59547b498b9f1cbd998ebfc68a406bac9bcd
Benchen997 <benchen.melbournechen0422@gmail.com>
2024-08-27 15:00:13 +1000

    migrate: oAuth login in frontent to turbo repo

--- apps/client/src/app/(protected)/dashboard/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-import { DashboardRoute } from "@src/module/dashboard";
-import { ProfileCard } from "@src/module/dashboard/profile/profile-card";
-
-export const metadata = DashboardRoute.Profile.Metadata;
-
-export default function Page() {
-  return <ProfileCard />;
-}
