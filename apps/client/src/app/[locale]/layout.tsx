594d75b4dc8a50e17a195e48b12ad0453eaec131
norriswu0 <norris.wu.au+github@outlook.com>
2024-08-28 23:20:27 +1000

    clean out old theme scripts

--- apps/client/src/app/[locale]/layout.tsx
+++ apps/client/src/app/[locale]/layout.tsx
@@ -18,20 +18,6 @@ export default async function RootLayout({
           rel="stylesheet"
           href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                try {
-                  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                    document.documentElement.classList.add('dark')
-                    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0B1120')
-                  } else {
-                    document.documentElement.classList.remove('dark')
-                  }
-                } catch (_) {}
-              `,
-          }}
-        />
       </head>
       <body className="dark:bg-primary-800 text-primary-700 bg-primary-100 dark:text-primary-100 font-sans antialiased transition-colors">
         <NextIntlClientProvider messages={messages}>
