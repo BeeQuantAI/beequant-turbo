b3c7be125110eb0a784078a31bcc8a46238297eb
Flora Li <samantha20200604@gmail.com>
2024-08-29 22:47:22 +1000

    fix the wrong router of not-found page add next/intl for not-found page

--- apps/client/src/app/[locale]/not-found.tsx
+++ apps/client/src/app/[locale]/not-found.tsx
@@ -1,10 +1,11 @@
 import { AuthRoute } from "@src/module/auth";
-import { NotFound } from "@src/module/common/not-found-page";
+import { NotFoundContent } from "@src/module/common/not-found-page";
+import { useTranslations } from "next-intl";
 
 export const metadata = AuthRoute.PageNotFound.Metadata;
 
-export default function Page() {
-  return (
-    <NotFound message="Ooops! The page you are looking for could not be found" />
-  );
+export default function NotFoundPage() {
+  const t = useTranslations("NotFoundPage");
+
+  return <NotFoundContent message={t("content.message")} />;
 }
--- apps/client/src/module/common/not-found-page.tsx
+++ apps/client/src/module/common/not-found-page.tsx
@@ -2,12 +2,15 @@
 
 import { Button } from "@src/module/common";
 import { AuthRoute } from "@src/module/auth";
+import { useTranslations } from "next-intl";
 
 interface NotFoundProps {
   message: string;
 }
 
-export function NotFound({ message }: NotFoundProps) {
+export function NotFoundContent({ message }: NotFoundProps) {
+  const t = useTranslations();
+
   return (
     <main>
       <div className="dark:bg-primary-800 bg-primary-50 flex h-screen overflow-auto bg-[url('/image/bg_404.png')] bg-cover bg-center bg-no-repeat text-center">
@@ -19,7 +22,7 @@ export function NotFound({ message }: NotFoundProps) {
           />
           <div className="mb-5 mt-[5.625rem] text-[1.1875rem]">{message}</div>
           <AuthRoute.Login.Link>
-            <Button variant="default">Back Home</Button>
+            <Button variant="default">{t("NotFoundPage.backHome")}</Button>
           </AuthRoute.Login.Link>
         </div>
       </div>
