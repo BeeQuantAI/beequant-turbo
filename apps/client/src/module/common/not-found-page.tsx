f9ff149559c7e35b35df7fbc17b498b67f85a443
Flora Li <samantha20200604@gmail.com>
2024-09-03 11:44:10 +1000

    upload images and their routes

--- apps/client/src/module/common/not-found-page.tsx
+++ apps/client/src/module/common/not-found-page.tsx
@@ -13,10 +13,10 @@ export function NotFoundContent({ message }: NotFoundProps) {
 
   return (
     <main>
-      <div className="dark:bg-primary-800 bg-primary-50 flex h-screen overflow-auto bg-[url('/image/bg_404.png')] bg-cover bg-center bg-no-repeat text-center">
+      <div className="dark:bg-primary-800 bg-primary-50 flex h-screen overflow-auto bg-[url('/img/bg_404.png')] bg-cover bg-center bg-no-repeat text-center">
         <div className="m-auto p-[0.625rem]">
           <img
-            src="/image/404.png"
+            src="/img/404.png"
             alt="404"
             className="w-full max-w-[31.25rem]"
           />
