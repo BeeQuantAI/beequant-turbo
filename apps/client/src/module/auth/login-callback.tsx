2c78db012d0844db33942bcb16ebde4a33b62e62
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-10-08 15:59:09 +0930

    Feat: Fix the background colour at LoginCallback page
    Completed following features:
    - Fixed the wrong background colour
    
    Resolve: CP-152

--- apps/client/src/module/auth/login-callback.tsx
+++ apps/client/src/module/auth/login-callback.tsx
@@ -1,25 +1,14 @@
 "use client";
-import { useTheme } from "@src/module/system";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { fetchUserInfo } from "@src/module/auth/user-store";
 import { useRouter } from "@src/configs/navigation";
 import { AuthRoute } from "@src/module/auth/route";
+import { Loading } from "../common/loading-animation";
 
 export function LoginCallback({ token }: { token: string }) {
-  const getTheme = useTheme();
   const router = useRouter();
   const [OAuthLogin, setOAuthLogin] = useState(false);
-  const [theme, setTheme] = useState("");
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const { theme } = getTheme;
-      if (theme) {
-        setTheme(theme);
-      }
-    }
-  }, []);
 
   useEffect(() => {
     if (token && typeof window !== "undefined") {
@@ -40,20 +29,5 @@ export function LoginCallback({ token }: { token: string }) {
     }
   }, [token, router]);
 
-  const backgroundColor = theme === "dark" ? "bg-black" : "bg-white";
-  const textColor = theme === "dark" ? "text-white" : "text-black";
-
-  return (
-    <div
-      className={`fixed inset-0 z-50 flex flex-col items-center justify-center ${backgroundColor} bg-opacity-50`}
-    >
-      <p className={`mb-4 text-center text-lg ${textColor}`}>
-        Taking you to the dashboard...
-      </p>
-      <div
-        className="inline-block h-16 w-16 animate-spin rounded-full border-8 border-solid border-blue-500 border-t-transparent"
-        role="status"
-      ></div>
-    </div>
-  );
+  return <Loading redirectFrom={"login-callback"} />;
 }
